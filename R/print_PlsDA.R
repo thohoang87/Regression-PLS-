c19d88364b649c14ea4a9507fb2c94fdca744e82
Samibgh <78345903+Samibgh@users.noreply.github.com>
2022-11-19 19:44:26 +0100

    change

--- R/print_PlsDA.R
+++ R/print_PlsDA.R
@@ -18,8 +18,11 @@ print.PLSDA=function(objet){
   # Affichage
   cat("Nombres de composantes : ",objet$n_components,"\n")
   cat("Classification functions","\n")
-  temp=as.matrix(objet$calc$Coefficients)
-  classement = sapply(1:length(objet$calc$Intercept),function(j){temp[,j]+objet$calc$Intercept[j]})
-  colnames(classement)=paste(colnames(objet$calc$Y))
-  print(classement)
+  colnames(objet$calc$Coefficients) = colnames(objet$calc$Y)
+  rownames(objet$calc$Coefficients) = colnames(objet$calc$X)
+  classification = rbind(objet$calc$Coefficients,objet$calc$Intercept)
+
+  print(classification)
 }
+
+
